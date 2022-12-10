02cd8826c0fc0b369f8b9a2369868146af127cc5
erees1 <edward.r.rees@zoho.com>
2022-12-10 14:17:46 +0000

    update to python3

--- export_events.py
+++ export_events.py
@@ -1,119 +1,122 @@
-import time
-import datetime
 import json
 import os
 import os.path
-import sys
 import glob
+import logging
 
 
-def loadEvents(fname):
-  """
-  Reads a file that consists of first column of unix timestamps
-  followed by arbitrary string, one per line. Outputs as dictionary.
-  Also keeps track of min and max time seen in global mint,maxt
-  """
-  events = []
+LOG_TYPES = ["window", "keyfreq", "notes", "blog"]
+ROOT = ""
+RENDER_ROOT = os.path.join(ROOT, "render", "event_jsons")
 
-  try:
-    ws = open(fname, 'r').read().decode('utf-8').splitlines()
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
+
+
+def load_events(fname):
+    """
+    Reads a file that consists of first column of unix timestamps
+    followed by arbitrary string, one per line. Outputs as dictionary.
+    Also keeps track of min and max time seen in global mint,maxt
+    """
+
     events = []
-    for w in ws:
-      ix = w.find(' ') # find first space, that's where stamp ends
-      stamp = int(w[:ix])
-      str = w[ix+1:]
-      events.append({'t':stamp, 's':str})
-  except Exception, e:
-    print '%s probably does not exist, setting empty events list.' % (fname, )
-    print 'error was:'
-    print e
-    events = []
-  return events
+    with open(fname, "r") as fh:
+        for w in fh:
+            w = w.strip()
+            ix = w.find(" ")  # find first space, that's where stamp ends
+            stamp = int(w[:ix])
+            str = w[ix + 1 :]
+            events.append({"t": stamp, "s": str})
+    return events
+
 
 def mtime(f):
-  """
-  return time file was last modified, or 0 if it doesnt exist
-  """
-  if os.path.isfile(f):
-    return int(os.path.getmtime(f))
-  else:
-    return 0
-
-def updateEvents():
-  """
-  goes down the list of .txt log files and writes all .json
-  files that can be used by the frontend
-  """
-  L = []
-  L.extend(glob.glob("logs/keyfreq_*.txt"))
-  L.extend(glob.glob("logs/window_*.txt"))
-  L.extend(glob.glob("logs/notes_*.txt"))
-
-  # extract all times. all log files of form {type}_{stamp}.txt
-  ts = [int(x[x.find('_')+1:x.find('.txt')]) for x in L]
-  ts = list(set(ts))
-  ts.sort()
-
-  mint = min(ts)
-  maxt = max(ts)
-
-  # march from beginning to end, group events for each day and write json
-  ROOT = ''
-  RENDER_ROOT = os.path.join(ROOT, 'render', 'event_jsons')
-  os.system('mkdir -p ' + RENDER_ROOT) # make sure output directory exists
-  t = mint
-  out_list = []
-  for t in ts:
-    t0 = t
-    t1 = t0 + 60*60*24 # 24 hrs later
-    fout = 'events_%d.json' % (t0, )
-    out_list.append({'t0':t0, 't1':t1, 'fname': fout})
-
-    fwrite = os.path.join(RENDER_ROOT, fout)
-    e1f = 'logs/window_%d.txt' % (t0, )
-    e2f = 'logs/keyfreq_%d.txt' % (t0, )
-    e3f = 'logs/notes_%d.txt' % (t0, )
-    e4f = 'logs/blog_%d.txt' % (t0, )
-
-    dowrite = False
-
-    # output file already exists?
-    # if the log files have not changed there is no need to regen
-    if os.path.isfile(fwrite):
-      tmod = mtime(fwrite)
-      e1mod = mtime(e1f)
-      e2mod = mtime(e2f)
-      e3mod = mtime(e3f)
-      e4mod = mtime(e4f)
-      if e1mod > tmod or e2mod > tmod or e3mod > tmod or e4mod > tmod:
-        dowrite = True # better update!
-        print 'a log file has changed, so will update %s' % (fwrite, )
+    """
+    return time file was last modified, or 0 if it doesnt exist
+    """
+    if os.path.isfile(f):
+        return int(os.path.getmtime(f))
     else:
-      # output file doesnt exist, so write.
-      dowrite = True
+        return 0
 
-    if dowrite:
-      # okay lets do work
-      e1 = loadEvents(e1f)
-      e2 = loadEvents(e2f)
-      e3 = loadEvents(e3f)
-      for k in e2:
-        try: 
-          k['s'] = int(k['s']) # int convert
-        except Exception:
-          print 'Error on %s' % k
-      e4 = ''
-      if os.path.isfile(e4f):
-        e4 = open(e4f, 'r').read()
 
-      eout = {'window_events': e1, 'keyfreq_events': e2, 'notes_events': e3, 'blog': e4}
-      open(fwrite, 'w').write(json.dumps(eout))
-      print 'wrote ' + fwrite
+def update_events(force=False):
+    """
+    goes down the list of .txt log files and writes all .json
+    files that can be used by the frontend
+    """
+    logging.info("updating events")
+    L = []
+    for log_type in LOG_TYPES:
+        L.extend(glob.glob(f"logs/{log_type}_*.txt"))
 
-  fwrite = os.path.join(RENDER_ROOT, 'export_list.json')
-  open(fwrite, 'w').write(json.dumps(out_list).encode('utf8'))
-  print 'wrote ' + fwrite
+    # extract all times. all log files of form {type}_{stamp}.txt
+    ts = [int(x[x.find("_") + 1 : x.find(".txt")]) for x in L]
+    ts = list(set(ts))
+    ts.sort()
+
+    mint = min(ts)
+
+    # march from beginning to end, group events for each day and write json
+    os.system("mkdir -p " + RENDER_ROOT)  # make sure output directory exists
+    t = mint
+    out_list = []
+    for t in ts:
+        t0 = t
+        t1 = t0 + 60 * 60 * 24  # 24 hrs later
+        fout = "events_%d.json" % (t0,)
+        out_list.append({"t0": t0, "t1": t1, "fname": fout})
+
+        fwrite = os.path.join(RENDER_ROOT, fout)
+
+        # log_files_for_day will be dictionary with key as log type and value as log file name
+        log_files_for_day = {}
+        for log_type in LOG_TYPES:
+            log_file_for_day = f"logs/{log_type}_{t0}.txt"
+            if os.path.isfile(log_file_for_day):
+                log_files_for_day[log_type] = log_file_for_day
+
+        dowrite = False
+        # output file already exists?
+        # if the log files have not changed there is no need to regen
+        if os.path.isfile(fwrite):
+            tmod = mtime(fwrite)
+            mod_times = [mtime(x) for x in log_files_for_day.values()]
+            if max(mod_times) > tmod:
+                dowrite = True  # better update!
+                logging.info(f"a log file has changed, so will update {fwrite}")
+        else:
+            # output file doesnt exist, so write.
+            dowrite = True
+
+        if dowrite or force:
+            # okay lets do work, frontend expects a dictionary with keys with suffix _events
+            events = {f"{k}_events": load_events(x) for k, x in log_files_for_day.items() if k != "blog"}
+            if "keyfreq_events" in events:
+                for key_freq_entry in events["keyfreq_events"]:
+                    try:
+                        key_freq_entry["s"] = int(key_freq_entry["s"])  # int convert
+                    except Exception:
+                        print(f"Error on {key_freq_entry}")
+
+            if "blog" in log_files_for_day:
+                with open(log_files_for_day["blog"], "r") as fh:
+                    events["blog"] = fh.read()
+
+            for k in LOG_TYPES:
+                if k != "blog" and k + "_events" not in events:
+                    events[k + "_events"] = []
+
+            with open(fwrite, "w") as fh:
+                fh.write(json.dumps(events))
+            logging.info("wrote " + fwrite)
+
+    fwrite = os.path.join(RENDER_ROOT, "export_list.json")
+    with open(fwrite, "w") as fh:
+        fh.write(json.dumps(out_list))
+    logging.info("wrote " + fwrite)
+
 
 # invoked as script
-if __name__ == '__main__':
-  updateEvents()
+if __name__ == "__main__":
+    update_events(force=True)
--- ulogme_serve.py
+++ ulogme_serve.py
@@ -1,74 +1,74 @@
-import SocketServer
-import SimpleHTTPServer
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 import sys
 import cgi
 import os
 
-from export_events import updateEvents
-from rewind7am import rewindTime
+from export_events import update_events
 
 # Port settings
 IP = ""
 if len(sys.argv) > 1:
-  PORT = int(sys.argv[1])
+    PORT = int(sys.argv[1])
 else:
-  PORT = 8124
+    PORT = 8124
 
 # serve render/ folder, not current folder
 file_path = os.path.realpath(__file__)
 rootdir = os.path.dirname(file_path)
-os.chdir(rootdir + '/render')
+os.chdir(rootdir + "/render")
+
+
 # Custom handler
-class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-  def do_GET(self):
-    # default behavior
-    SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self) 
+class CustomHandler(SimpleHTTPRequestHandler):
+    def do_POST(self):
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={
+                "REQUEST_METHOD": "POST",
+                "CONTENT_TYPE": self.headers["Content-Type"],
+            },
+        )
+        result = "NOT_UNDERSTOOD"
 
-  def do_POST(self):
-    form = cgi.FieldStorage(
-      fp = self.rfile,
-      headers = self.headers,
-      environ = {'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type']})
-    result = 'NOT_UNDERSTOOD'
+        if self.path == "/refresh":
+            print("refresh button pressed")
+            # recompute jsons. We have to pop out to root from render directory
+            # temporarily. It's a little ugly
+            os.chdir(rootdir)  # pop out
+            update_events()  # defined in export_events.py
+            os.chdir("render")  # pop back to render directory
+            result = "OK"
 
-    if self.path == '/refresh':
-      # recompute jsons. We have to pop out to root from render directory
-      # temporarily. It's a little ugly
-      refresh_time = form.getvalue('time')
-      os.chdir(rootdir) # pop out
-      updateEvents() # defined in export_events.py
-      os.chdir('render') # pop back to render directory
-      result = 'OK'
-      
-    if self.path == '/addnote':
-      # add note at specified time and refresh
-      note = form.getvalue('note')
-      note_time = form.getvalue('time')
-      os.chdir(rootdir) # pop out
-      os.system('echo %s | ./note.sh %s' % (note, note_time))
-      updateEvents() # defined in export_events.py
-      os.chdir('render') # go back to render
-      result = 'OK'
+        if self.path == "/addnote":
+            # add note at specified time and refresh
+            note = form.getvalue("note")
+            note_time = form.getvalue("time")
+            os.chdir(rootdir)  # pop out
+            os.system("echo %s | ./note.sh %s" % (note, note_time))
+            update_events()  # defined in export_events.py
+            os.chdir("render")  # go back to render
+            result = "OK"
 
-    if self.path == '/blog':
-      # add note at specified time and refresh
-      post = form.getvalue('post')
-      if post is None: post = ''
-      post_time = int(form.getvalue('time'))
-      os.chdir(rootdir) # pop out
-      trev = rewindTime(post_time)
-      open('logs/blog_%d.txt' % (post_time, ), 'w').write(post)
-      updateEvents() # defined in export_events.py
-      os.chdir('render') # go back to render
-      result = 'OK'
-    
-    self.send_response(200)
-    self.send_header('Content-type','text/html')
-    self.end_headers()
-    self.wfile.write(result)
+        if self.path == "/blog":
+            # add note at specified time and refresh
+            post = form.getvalue("post")
+            if post is None:
+                post = ""
+            post_time = int(form.getvalue("time"))
+            os.chdir(rootdir)  # pop out
+            open("logs/blog_%d.txt" % (post_time,), "w").write(post)
+            update_events()  # defined in export_events.py
+            os.chdir("render")  # go back to render
+            result = "OK"
 
-httpd = SocketServer.ThreadingTCPServer((IP, PORT), CustomHandler)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(result.encode("utf-8"))
 
-print 'Serving ulogme, see it on http://localhost:' + `PORT`
+
+httpd = ThreadingHTTPServer((IP, PORT), CustomHandler)
+
+print("Serving ulogme, see it on http://localhost:" + f"{PORT}")
 httpd.serve_forever()
-
