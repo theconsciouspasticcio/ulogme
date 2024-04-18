f7db327f0074c2b9bb647e69cec6a40923e8a8bb
Donato <theconsciouspasticcio@gmail.com>
2024-04-18 16:01:41 +0200

    emacs washed

--- render/js/render_settings.js
+++ render/js/render_settings.js
@@ -6,55 +6,64 @@
 // may get mapped to just "Google Chrome".
 // these get applied in order they are specified, from top to bottom
 var title_mappings = [
-  { pattern: /Google Chrome/, mapto: "Google Chrome" },
+  // { pattern: /Google Chrome/, mapto: "Google Chrome" },
   { pattern: /Firefox/, mapto: "Google Chrome" }, // lol
-  { pattern: /youtube/, mapto: "Tha Tube" }, // lol
-  { pattern: /Inotebook/, mapto: "Coding" },
+  { pattern: /YouTube/, mapto: "Learning" },
+  // { pattern: /TDA/, mapto: "Learning" }, // lol
+  { pattern: /vterm/, mapto: "Coding" },
   { pattern: /\/lab/, mapto: "Coding" },
-  { pattern: /colab.research.google/, mapto: "Coding" },
+  // { pattern: /colab.research.google/, mapto: "Coding" },
   { pattern: /.pdf/, mapto: "Papers" },
-  { pattern: /Preview/, mapto: "Learning" },
-  { pattern: /Mendeley/, mapto: "Papers" },
-  { pattern: /Teams/, mapto: "Meetings" },
-  { pattern: /edwardr@/, mapto: "Coding" },
-  { pattern: /iTerm2/, mapto: "Coding" },
+  // { pattern: /Preview/, mapto: "Learning" },
+  // { pattern: /Mendeley/, mapto: "Papers" },
+  { pattern: /Zoom/, mapto: "Meetings" },
+  // { pattern: /edwardr@/, mapto: "Coding" },
+  // { pattern: /iTerm2/, mapto: "Coding" },
   { pattern: /Code/, mapto: "Coding" },
   { pattern: /__LOCKEDSCREEN/, mapto: "Locked Screen" }, // __LOCKEDSCREEN is a special token
   { pattern: /ScreenSaverEngine/, mapto: "Locked Screen" },
   { pattern: /loginwindow/, mapto: "Locked Screen" },
 
-  { pattern: /TeXworks/, mapto: "Latex" },
-  { pattern: /localhost/, mapto: "Other Work" },
-  { pattern: /aml./, mapto: "Other Work" },
-  { pattern: /Speechmatics/, mapto: "Other Work" },
+  { pattern: /.org/, mapto: "Org" },
+  { pattern: /.tex/, mapto: "Latex" },
+
+  // { pattern: /localhost/, mapto: "Other Work" },
   { pattern: /blog/, mapto: "Learning" },
-  { pattern: /TickTick/, mapto: "Admin" },
-  { pattern: /Outlook/, mapto: "Admin" },
+  // { pattern: /TickTick/, mapto: "Admin" },
+  // { pattern: /Outlook/, mapto: "Admin" },
 
-  { pattern: /jamboard/, mapto: "Other Work" },
-  { pattern: /atlassian/, mapto: "Other Work" },
-  { pattern: /scalars/, mapto: "Other Work" },
-  { pattern: /git/, mapto: "Other Work" },
+  { pattern: /git/, mapto: "Coding" },
 
-  { pattern: /youtube/, mapto: "Time Wasting" },
+  { pattern: /\b(guide|review|ebay)\b/i , mapto: "Time Wasting" },
+  { pattern: /Review/, mapto: "Time Wasting" },
+  { pattern: /Discord/, mapto: "Time Wasting" },
   { pattern: /ebay/, mapto: "Time Wasting" },
-  { pattern: /Spotify/, mapto: "Time Wasting" },
-  { pattern: /localhost:8124/, mapto: "Time Wasting" }, // meta
+  { pattern: /Instagram/, mapto: "Time Wasting" },
+  { pattern: /ulogme/, mapto: "Time Wasting" }, // meta
+  { pattern: /localhost/, mapto: "Time Wasting" }, // meta
+  { pattern: /Minibuf/, mapto: "Time Wasting" },
 
   { pattern: /pytorch/, mapto: "Coding" },
   { pattern: /python/, mapto: "Coding" },
-  { pattern: /stackoverflow/, mapto: "Coding" },
+  { pattern: /.py/, mapto: "Coding" },
+  { pattern: /.el/, mapto: "Coding" },
+  { pattern: /.js/, mapto: "Coding" },
+  { pattern: /.clj/, mapto: "Coding" },
+
+  { pattern: /stackoverflow/, mapto: "Learning" },
 
   { pattern: /lesswrong/, mapto: "Learning" },
-  { pattern: /mlscaling/, mapto: "Learning" },
+  // { pattern: /mlscaling/, mapto: "Learning" },
   { pattern: /matter/, mapto: "Learning" },
   { pattern: /Anki/, mapto: "Anki" },
-  { pattern: /Obsidian/, mapto: "Learning" },
-  { pattern: /agi/, mapto: "Learning" },
-  { pattern: /align/, mapto: "Learning" },
-  { pattern: /RobertMiles/, mapto: "Learning" },
+  { pattern: /math/, mapto: "Learning" },
+  { pattern: /Logseq/, mapto: "Learning" },
+  // { pattern: /agi/, mapto: "Learning" },
+  // { pattern: /align/, mapto: "Learning" },
+  { pattern: /substack/, mapto: "Learning" },
   { pattern: /lilianweng/, mapto: "Learning" },
   { pattern: /karpathy/, mapto: "Learning" },
+  { pattern: /elfeed/, mapto: "Learning" },
 ];
 
 // be very careful with ordering in the above because titles
@@ -83,19 +92,18 @@ function mapwin(w) {
 // to group my work stuff and play stuff together.
 var display_groups = [];
 // Non work related
-display_groups.push(["Coding", "Papers", "Other Work", "Admin", "Meetings"]); // work related
-display_groups.push(["Learning", "Anki"]); // Personal development
-display_groups.push(["Google Chrome", "MISC"]); // internet related
+display_groups.push(["Meetings"]); // work related
+display_groups.push(["Learning", "Papers", "Coding", "Org"]); // Personal development
+display_groups.push(["Google Chrome", "MISC", "Time Wasting"]); // internet related
 // display_groups.push(["TeXworks"]); // paper writing related
 display_groups.push(["Locked Screen"]); // computer not being used
-display_groups.push(["Time Wasting"]);
 // list of titles that classify as "hacking", or being productive in general
 // the main goal of the day is to get a lot of focused sessions of hacking
 // done throughout the day. Windows that arent in this list do not
 // classify as hacking, and they break "streaks" (events of focused hacking)
 // the implementation is currently quite hacky, experimental and contains
 // many magic numbers.
-var hacking_titles = ["Coding"];
+var hacking_titles = ["Coding", "Papers", "Org"];
 var draw_hacking = true; // by default turning this off
 
 // draw notes row?
@@ -106,12 +114,12 @@ var draw_notes = true;
 // levels of coffee in body over time
 var draw_coffee = true;
 
-var key_links = [];
-key_links.push({
-  key: "Work",
-  link: "obsidian://open?vault=md-notes&file=Work%20main",
-});
-key_links.push({
-  key: "Dev",
-  link: "obsidian://open?vault=md-notes&file=Dev%20main",
-});
+// var key_links = [];
+// key_links.push({
+//   key: "Work",
+//   link: "obsidian://open?vault=md-notes&file=Work%20main",
+// });
+// key_links.push({
+//   key: "Dev",
+//   link: "obsidian://open?vault=md-notes&file=Dev%20main",
+// });
