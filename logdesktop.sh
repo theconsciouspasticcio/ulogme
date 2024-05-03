381afa1c821810394bcb06c959ee15b2453be2c0
Donato <theconsciouspasticcio@gmail.com>
2024-05-03 12:58:32 +0200

    Fri May  3 12:58:27 PM CEST 2024

--- logdesktop.sh
+++ logdesktop.sh
@@ -13,18 +13,18 @@ mkdir -p desktopscr
 
 while true
 do
-	islocked=true; if [[ $(gnome-screensaver-command -q) =~ .*inactive.* ]]; then islocked=false; fi
+	# islocked=true; if [[ $(gnome-screensaver-command -q) =~ .*inactive.* ]]; then islocked=false; fi
 
-	if ! $islocked
-	then
+	# if ! $islocked
+	# then
 		# take screenshot into file
 		T="$(date +%s)"
 		fname="$saveprefix_$T.jpg"
 		# q is quality. Higher is higher quality
 		scrot -q 50 "$fname"
-	else
-		echo "screen is locked, waiting..."
-	fi
+	# else
+	# 	echo "screen is locked, waiting..."
+	# fi
 
 	sleep $waittime
 done
